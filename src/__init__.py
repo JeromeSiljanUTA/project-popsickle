aba1d45061cbce145433391f97b03fa11ad4f9d2
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-07 21:28:10 -0600

    feat(src/__init__.py): optionally load from db

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,13 @@
 """
 This module calls other modules to clean and display our data and predictions
 """
-from d01_data.load_data import import_files
+from d01_data.load_data import import_files, load_from_db
 from d02_intermediate.manage_db import insert_data
 
-df = import_files()
-insert_data(df)
+DB_POPULATED = True
+
+if DB_POPULATED:
+    df = load_from_db()
+else:
+    df = import_files()
+    insert_data(df)
