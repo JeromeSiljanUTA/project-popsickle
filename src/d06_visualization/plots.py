f2580dc974f7c16a7d81d0bf7d2b77768ea60b4e
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-12 18:15:40 -0600

    feat(src/d06_visualization/plots.py): plots a region's weather for a specific year

--- src/d06_visualization/plots.py
+++ src/d06_visualization/plots.py
@@ -1,8 +1,13 @@
+import datetime as dt
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
 
 
-def plotly_weather(weather_df):
-    weather_gb = weather_df.groupby("City")
+def weather_region_year(df, region, year):
+    region_df = df[df["Region"] == region]
+    year_df = region_df[region_df["Time"].dt.year == year]
+    fig = px.line(year_df, x="Time", y="Temperature", color="City")
+    fig.show()
