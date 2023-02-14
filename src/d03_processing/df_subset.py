61884c1a756685c043d7da00be7bdc817d5d72b8
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-14 16:37:21 -0600

    feat(src/d03_processing/df_subset.py): added get_subset()

---
+++ src/d03_processing/df_subset.py
@@ -0,0 +1,26 @@
+import datetime as dt
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+
+
+def get_subset(df, city=None, region=None, year=None):
+    if region is not None:
+        if isinstance(region, list):
+            df = df[df["Region"].isin(region)]
+        else:
+            df = df[df["Region"] == region]
+    if year is not None:
+        if isinstance(year, list):
+            df = df[df["Time"].dt.year.isin(year)]
+        else:
+            df = df[df["Time"].dt.year == year]
+    if city is not None:
+        if isinstance(city, list):
+            df = df[df["City"].isin(city)]
+        else:
+            df = df[df["City"] == city]
+
+    return df
