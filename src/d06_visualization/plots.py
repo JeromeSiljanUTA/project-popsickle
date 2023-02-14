30d9d0865af348f1de4321d4f424084c5a48164a
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-14 16:38:12 -0600

    feat(src/d06_visualization/plots.py): makes use of get_subset() from d03_processing

--- src/d06_visualization/plots.py
+++ src/d06_visualization/plots.py
@@ -4,24 +4,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
+from d03_processing.df_subset import get_subset
 
 
 def plot_weather(df, city=None, region=None, year=None):
-    if region is not None:
-        if isinstance(region, list):
-            df = df[df["Region"].isin(region)]
-        else:
-            df = df[df["Region"] == region]
-    if year is not None:
-        if isinstance(year, list):
-            df = df[df["Time"].dt.year.isin(year)]
-        else:
-            df = df[df["Time"].dt.year == year]
-    if city is not None:
-        if isinstance(city, list):
-            df = df[df["City"].isin(city)]
-        else:
-            df = df[df["City"] == city]
+    df = get_subset(df, city, region, year)
 
     fig = px.line(df, x="Time", y="Temperature", color="City")
     fig.show()
