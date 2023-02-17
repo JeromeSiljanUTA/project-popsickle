7f992835c7ec40bed24a9cd6249c33815e3c4ce9
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-16 18:31:56 -0600

    feat(src/d06_visualization/plots.py): added plot_power()

--- src/d06_visualization/plots.py
+++ src/d06_visualization/plots.py
@@ -12,3 +12,11 @@ def plot_weather(df, city=None, region=None, year=None):
 
     fig = px.line(df, x="Time", y="Temperature", color="City")
     fig.show()
+
+
+def plot_power(df, city=None, region=None, year=None):
+    df = get_subset(df, city, region, year)
+    y_list = df.columns.to_list()
+    y_list.remove("Time")
+    fig = px.line(df, x="Time", y=y_list)
+    fig.show()
