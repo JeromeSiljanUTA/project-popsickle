677b8d0dba4084d7c794054791357d6464851f21
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-02-17 18:25:42 -0600

    feat(src/d06_visualization/plots.py): Plots daily autocorrelation for power_df

--- src/d06_visualization/plots.py
+++ src/d06_visualization/plots.py
@@ -9,7 +9,6 @@ from d03_processing.df_subset import get_subset
 
 def plot_weather(df, city=None, region=None, year=None):
     df = get_subset(df, city, region, year)
-
     fig = px.line(df, x="Time", y="Temperature", color="City")
     fig.show()
 
@@ -19,4 +18,10 @@ def plot_power(df, city=None, region=None, year=None):
     y_list = df.columns.to_list()
     y_list.remove("Time")
     fig = px.line(df, x="Time", y=y_list)
+    fig.show()
+
+
+def plot_daily_autocorr_power(df):
+    autocorr = [df["ERCOT"].autocorr(n) for n in range(0, 48)]
+    fig = px.line(x=range(0, 48), y=autocorr)
     fig.show()
