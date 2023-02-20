b388a3d8d6bdad6a8346eb599019fc487df81584
JeromeSiljanUTA <JeromeSiljanUTA>
2023-02-19 22:50:18 -0600

    feat(src/__init__.py): Added train *arima* models

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,8 @@ from d01_data.get_weather import get_weather_city
 from d01_data.load_data import import_files, load_from_db
 from d02_intermediate.manage_db import insert_power_data, insert_weather_data
 from d03_processing.df_subset import get_subset
+from d04_modelling.arima import fit_arima, threaded_arima
+from d04_modelling.sarimax import fit_sarimax
 from d06_visualization.plots import plot_power, plot_weather
 
 # Initialize argparse parser
@@ -47,3 +49,13 @@ if not WEATHER_DATA_POPULATED:
 
 power_df = load_from_db(POWER_DATA)
 weather_df = load_from_db(WEATHER_DATA)
+
+
+def train_arimas():
+    orders = [(24, 0, 1), (24, 1, 1)]
+
+
+def train_sarimax():
+    seasonal_order = (4, 0, 1, 24)
+    order = (0, 1, 0)
+    fit_sarimax(power_df, seasonal_order, order)
